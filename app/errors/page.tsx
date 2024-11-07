dc23bf52cdc7ad33e3585896020bd76afd90e235
Vincent C <vinch.cm@gmail.com>
2024-11-07 13:49:51 +0100

    Error component fixed

--- app/errors/page.tsx
+++ app/errors/page.tsx
@@ -1,3 +1,5 @@
+import Error from "../error";
+
 export default async function ErrorPage() {
     try {
     const response = await fetch('https://restcountries.com/v4.1/all');
@@ -5,7 +7,7 @@ export default async function ErrorPage() {
     if (response.ok) {
       console.log('Promise resolved and HTTP status is successful');
     } else {
-      return Error;
+      return <Error />;
     }
   } catch (error) {
     console.error('Fetch', error);
