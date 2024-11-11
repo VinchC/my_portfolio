fed80b1006c3619b66322a178319e4b372263ddb
Vincent C <vinch.cm@gmail.com>
2024-11-11 13:24:40 +0100

    few fixes added

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -22,97 +22,111 @@ export const Header = ({
 
   return (
     <header className="fixed w-full top-0 py-4 h-16 bg-card border-b-[2px] border-background">
-      <Section className="flex">
-        <h1 className="text-lg font-bold text-card-foreground">
-          Vincent Cantonnet
-        </h1>
-        <div className="flex-1" />
+      <Section>
+        <nav className="flex">
+          <h1 className="text-lg font-bold text-card-foreground">
+            Vincent Cantonnet
+          </h1>
+          <div className="flex-1" />
 
-        <div className="flex-col">
-          <div className="md:hidden flex justify-end items-center">
-            <button
-              onClick={() => {
-                isOpen ? setIsOpen(false) : setIsOpen(true);
-              }}
-              type="button"
-              className="text-tertiary"
+          <div className="flex-col">
+            <div className="md:hidden flex justify-end items-center">
+              <button
+                onClick={() => {
+                  isOpen ? setIsOpen(false) : setIsOpen(true);
+                }}
+                type="button"
+                className="text-tertiary"
+              >
+                <Menu />
+              </button>
+            </div>
+            <div
+              className={
+                !isOpen
+                  ? "max-md:invisible"
+                  : "w-[100px] bg-card rounded-sm p-2 mr-0"
+              }
             >
-              <Menu />
-            </button>
+              <ul
+                className={`flex items-center gap-2 max-md:flex-col ${
+                  !isOpen ? "max-md:invisible" : "max-md:visible"
+                }`}
+              >
+                <Link
+                  href="#projects"
+                  className={cn(
+                    buttonVariants({ variant: "outline" }),
+                    "buttonLarge"
+                  )}
+                >
+                  {language === "English" ? "Projects" : "Projets"}
+                </Link>
+                <Link
+                  href="#work"
+                  className={cn(
+                    buttonVariants({ variant: "outline" }),
+                    "buttonLarge"
+                  )}
+                >
+                  {language === "English" ? "Work exp." : "Carrière"}
+                </Link>
+                <Link
+                  href="#soft"
+                  className={cn(
+                    buttonVariants({ variant: "outline" }),
+                    "buttonLarge"
+                  )}
+                >
+                  Soft skills
+                </Link>
+                <Link
+                  href="#contact"
+                  className={cn(
+                    buttonVariants({ variant: "outline" }),
+                    "buttonLarge"
+                  )}
+                >
+                  Contact
+                </Link>
+                <AccessibleIcon label="download CV">
+                  <CV />
+                </AccessibleIcon>
+                <button
+                  aria-label="switch the language between English and French"
+                  onClick={toggleLanguage}
+                  className={cn(
+                    buttonVariants({ variant: "outline" }),
+                    "buttonSmall max-md:buttonLarge"
+                  )}
+                >
+                  {language === "English" ? (
+                    <img
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Ensign_of_France.svg/langfr-225px-Ensign_of_France.svg.png"
+                      style={{ width: 16, height: "auto" }}
+                      alt="France flag"
+                    />
+                  ) : (
+                    <img
+                      src="https://upload.wikimedia.org/wikipedia/en/a/ae/Flag_of_the_United_Kingdom.svg"
+                      style={{ width: 16, height: "auto" }}
+                      alt="United Kingdom flag"
+                    />
+                  )}
+                </button>
+                <button
+                  aria-label="switch the mode from dark to light (os prefrences by default)"
+                  className={cn(
+                    buttonVariants({ variant: "outline" }),
+                    "buttonSmall max-md:buttonLarge"
+                  )}
+                >
+                  <ThemeSwitch />
+                </button>
+              </ul>
+            </div>
           </div>
-            <ul className={`flex items-center gap-2 max-md:flex-col ${!isOpen ? "max-md:invisible" : "max-md:visible"}`}>
-              <Link
-                href="#projects"
-                className={cn(
-                  buttonVariants({ variant: "outline" }),
-                  "buttonLarge"
-                )}
-              >
-                {language === "English" ? "Projects" : "Projets"}
-              </Link>
-              <Link
-                href="#work"
-                className={cn(
-                  buttonVariants({ variant: "outline" }),
-                  "buttonLarge"
-                )}
-              >
-                {language === "English" ? "Work exp." : "Carrière"}
-              </Link>
-              <Link
-                href="#soft"
-                className={cn(
-                  buttonVariants({ variant: "outline" }),
-                  "buttonLarge"
-                )}
-              >
-                Soft skills
-              </Link>
-              <Link
-                href="#contact"
-                className={cn(
-                  buttonVariants({ variant: "outline" }),
-                  "buttonLarge"
-                )}
-              >
-                Contact
-              </Link>
-              <AccessibleIcon label="download CV">
-                <CV />
-              </AccessibleIcon>
-              <button
-                aria-label="switch the language between English and French"
-                onClick={toggleLanguage}
-                className={cn(
-                  buttonVariants({ variant: "outline" }),
-                  "buttonSmall max-md:buttonLarge"
-                )}
-              >
-                {language === "English" ? (
-                  <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Ensign_of_France.svg/langfr-225px-Ensign_of_France.svg.png"
-                    style={{ width: 16, height: "auto" }}
-                    alt="France flag"
-                  />
-                ) : (
-                  <img
-                    src="https://upload.wikimedia.org/wikipedia/en/a/ae/Flag_of_the_United_Kingdom.svg"
-                    style={{ width: 16, height: "auto" }}
-                    alt="United Kingdom flag"
-                  />
-                )}
-              </button>
-              <button
-                aria-label="switch the mode from dark to light (os prefrences by default)"
-                className={cn(
-                  buttonVariants({ variant: "outline" }),
-                  "buttonSmall max-md:buttonLarge"
-                )}
-              >
-                <ThemeSwitch />
-              </button>
-            </ul>
-          </div>
+        </nav>
       </Section>
     </header>
   );
